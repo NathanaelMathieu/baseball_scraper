6c9cc778038dc0689bf725094d831f7b7279ec63
Nathanael Mathieu <nmathieu@wesleyan.edu>
2021-04-27 23:54:08 -0400

    Select the correct table

--- baseball_scraper/pitching_leaders.py
+++ baseball_scraper/pitching_leaders.py
@@ -12,7 +12,7 @@ def get_soup(start_season, end_season, league, qual, ind):
 
 def get_table(soup, ind):
     tables = soup.find_all('table')
-    table = tables[11]
+    table = tables[16]
     data = []
     # pulls headings from the fangraphs table
     headings = []
